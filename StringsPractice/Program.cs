779cdf1e424ce97ba6cb9bbf95c1f3263098b0bc
Joe Santoiemmo <JSanto333@gmail.com>
2017-06-29 15:07:08 -0400

    Completed several in-class exercises

--- StringsPractice/Program.cs
+++ StringsPractice/Program.cs
@@ -10,6 +10,68 @@ namespace StringsPractice
     {
         static void Main(string[] args)
         {
+            
+
+            //string myFaveQuote = "\"It's a yam sham\" - Buffy Summers";
+            //Console.WriteLine(myFaveQuote);
+
+            //string escapeString = "I want a tab after this line \t I want a new line without WriteLine \n Hi";
+            //string escapeString2 = "I want to show a backslash here \\ Hey what's that noise? \a";
+            //Console.WriteLine(escapeString);
+            //Console.WriteLine(escapeString2);
+
+            //Console.WriteLine("User one, please type something");
+            //string userOneInput = Console.ReadLine();
+            //Console.WriteLine("User two, please type something");
+            //string userTwoInput = Console.ReadLine();
+
+            //if (userOneInput == userTwoInput)
+            //{
+            //    Console.WriteLine("User 2 is a copy cat!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Way to be original!");
+            //}
+
+            //string startOfSentence = "My best friend's name is ";
+            //string endOfSentence = "Steph.";
+            //string fullSentence = startOfSentence + endOfSentence;
+
+            //Console.WriteLine(fullSentence);
+
+            //Console.WriteLine("This is a tab. \t \nThis is a backslash \\. \nAnd this is a bell sound\a.");
+
+            string firstName = "Joe";
+            int lengthOfName = firstName.Length;
+            Console.WriteLine(firstName);
+            Console.WriteLine(lengthOfName);
+
+            string lastName = "Santoiemmo";
+            int lengthOfLastName = lastName.Length;
+            Console.WriteLine(lastName);
+            Console.WriteLine(lengthOfLastName);
+
+            if (lengthOfName > lengthOfLastName)
+            {
+                Console.WriteLine("First is longer.");
+            }
+            if (lengthOfName == lengthOfLastName) 
+            {
+                Console.WriteLine("Samsies!");
+            }
+            else
+            {
+                Console.WriteLine("last must be longer!");
+            }
+
+            string myName = "Joe";
+            string oneName = "myName";
+            if {
+
+            }
+
+
         }
     }
 }
